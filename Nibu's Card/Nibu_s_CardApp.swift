b7bdbb63a2989a518ef68fb81af5316790780b62
nibualisons <nibu@alisonsgroup.com>
2022-11-18 15:28:43 +0530

    Initial Commit

---
+++ Nibu's Card/Nibu_s_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nibu_s_CardApp.swift
+//  Nibu's Card
+//
+//  Created by vybhav on 18/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Nibu_s_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
